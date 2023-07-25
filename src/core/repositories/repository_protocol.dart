d5ef4f69adf44ca7f8133556c8977908dda15771
AntonNel-ios <118670059+AntonNel-ios@users.noreply.github.com>
2023-07-25 22:03:39 +0300

    message

--- src/core/repositories/repository_protocol.dart
+++
@@ -1,6 +0,0 @@
-import '../model/responce.dart';
-import 'dto/question_dto.dart';
-
-abstract class RepositoryProtocol {
-  Future<ResponseModel> add(RequestDTO request);
-}
--- src/core/service/QuestionService.dart
+++
@@ -1,9 +0,0 @@
-import '../repositories/dto/question_dto.dart';
-import '../repositories/repository_protocol.dart';
-
-class QuestionService {
-  final RepositoryProtocol repository;
-  QuestionService({required this.repository});
-
-  getResponse({required RequestDTO request}) => repository.add(request);
-}
