e672765cb571172358321df74f6ccfc3a01dc86f
AntonNel-ios <118670059+AntonNel-ios@users.noreply.github.com>
2023-07-25 22:04:45 +0300

    //new logic

--- lib/screen/viewModel/MagicBallViewModel.dart
+++ lib/screen/viewModel/MagicBallViewModel.dart
@@ -1,15 +1,27 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:surf_practice_magic_ball/screen/viewModel/Logic/BallLogic.dart';
+import 'package:surf_practice_magic_ball/screen/viewModel/Logic/BallState.dart';
 import 'package:surf_practice_magic_ball/screen/viewModel/MagicBallViewModelState.dart';
 import 'package:surf_practice_magic_ball/src/core/repositories/dataSource/dataSource.dart';
 
 class MagicBallViewModel extends Cubit<MagicBallViewModelState> {
   late final BallLogic logic;
-
+  late final StreamSubscription<BallState> subscribe;
   final DataSourceProtocol repository;
 
   MagicBallViewModel(super.initialState, this.repository,
       {required BallLogic logicBloc}) {
     logic = logicBloc;
+    subscribe = logicBloc.stream.listen(onMapper);
+  }
+
+  Future<void> onMapper(BallState state) async {}
+
+  @override
+  Future<void> close() {
+    subscribe.cancel();
+    return super.close();
   }
 }
